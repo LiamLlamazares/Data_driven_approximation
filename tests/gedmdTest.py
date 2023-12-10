fedbc834bb2264f355af9855a9a97d3762d9a2e7
Liam Llamazares <s2266126@ed.ac.uk>
2023-12-10 23:42:04 +0100

    Data limit plot includes frobenius error

--- tests/gedmdTest.py
+++ tests/gedmdTest.py
@@ -12,11 +12,12 @@ import numpy.polynomial
 import scipy as sp
 import matplotlib.pyplot as plt
 import sys
-sys.path.insert(0, 'C:/Users/liaml/Documentos/gEDMD_code') # add path to gEDMD package, should be changed to your local path
+sys.path.insert(0, 'C:/Users/illam/Documents/GitHub/gEDMD_code') # add path to gEDMD package, should be changed to your local path
+sys.path.insert(0, 'C:/Users/illam/Documents/GitHub/gEDMD_code/d3s') # add path to d3s package, should be changed to your local path
 import d3s.algorithms as algorithms
 import d3s.domain as domain
 import d3s.observables as observables
-import d3s.systems as systems
+# import d3s.systems as systems
 
 from d3s.tools import printVector, printMatrix
 
