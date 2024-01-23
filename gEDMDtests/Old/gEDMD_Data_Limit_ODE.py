1a8d61f72437e9a6b2484d1afec172365e711b4a
Liam Llamazares <s2266126@ed.ac.uk>
2024-01-23 17:36:49 +0000

    Added plot helper to do data limit for general list of observables and general system. Makes it easier to use and not change multiple different files when updating methods for plotting and displaying data. Made loop faster by not calculating a different A_exact for each run.

--- gEDMDtests/Old/gEDMD_Data_Limit_ODE.py
+++ gEDMDtests/Old/gEDMD_Data_Limit_ODE.py
@@ -48,6 +48,8 @@ min_number_of_data_points = 200
 # generate data
 M = 200000
 confidence_level = 0.95
+number_of_runs = 225
+number_of_batches = 15
 
 number_of_loops = int(np.floor(np.log2(M / min_number_of_data_points))) + 1
 data_points_number = [
@@ -56,56 +58,55 @@ data_points_number = [
 print('max data_points_number = ',
       min_number_of_data_points * 2**number_of_loops, 'number_of_loops = ',
       number_of_loops)
-number_of_runs = 15
 
-operator_errors = np.zeros(
+matrix_errors = np.zeros(
     (number_of_loops, types_of_observables_number, number_of_runs))
-operator_errors_average = np.zeros(
+matrix_errors_average = np.zeros(
     (number_of_loops, types_of_observables_number))
 
 for m in range(number_of_runs):
     X_exact = Omega.rand(M)
     A_exact_m, _, _ = gedmd_helper.gedmdMatrices(X_exact, psi_m, b, Omega)
     A_exact_g, _, _ = gedmd_helper.gedmdMatrices(X_exact, psi_g, b, Omega)
-    A_exact_m__operator_norm = np.linalg.norm(A_exact_m, ord=2)
-    A_exact_g__operator_norm = np.linalg.norm(A_exact_g, ord=2)
+    A_exact_m__matrix_norm = np.linalg.norm(A_exact_m, ord=2)
+    A_exact_g__matrix_norm = np.linalg.norm(A_exact_g, ord=2)
     for i in range(number_of_loops):
         X = Omega.rand(data_points_number[i])
         A_m, _, _ = gedmd_helper.gedmdMatrices(X, psi_m, b, Omega)
         A_g, _, _ = gedmd_helper.gedmdMatrices(X, psi_g, b, Omega)
-        operator_error_m = np.linalg.norm(A_exact_m - A_m,
-                                          ord=2) / A_exact_m__operator_norm
-        operator_error_g = np.linalg.norm(A_exact_g - A_g,
-                                          ord=2) / A_exact_g__operator_norm
+        matrix_error_m = np.linalg.norm(A_exact_m - A_m,
+                                        ord=2) / A_exact_m__matrix_norm
+        matrix_error_g = np.linalg.norm(A_exact_g - A_g,
+                                        ord=2) / A_exact_g__matrix_norm
 
-        operator_errors[i, :, m] = [operator_error_m, operator_error_g]
+        matrix_errors[i, :, m] = [matrix_error_m, matrix_error_g]
         print('i = ', i, 'data points=', data_points_number[i],
               'run number = ', m)
 
-operator_errors_average = np.mean(operator_errors, axis=2)
-#calculate confidence intervals for the average error of the operators (95% confidence) for each number of data points
+matrix_errors_average = np.mean(matrix_errors, axis=2)
+#calculate confidence intervals for the average error of the matrices (95% confidence) for each number of data points
 #first we divide the error data into number_of_batches batches
-number_of_batches = 3
 batch_size = int(np.floor(number_of_runs /
                           number_of_batches))  #number of runs in each batch
-#GIves error if batch size is 0
+#Gives error if batch size is 0
 if batch_size == 0:
     raise Exception(
         'batch size is 0. Please increase number of runs or decrease number of batches'
     )
-operator_errors_batches = np.zeros(
+matrix_errors_batches = np.zeros(
     (number_of_loops, types_of_observables_number, number_of_batches))
-for i in range(number_of_batches):
-    operator_errors_batches[:, :, i] = np.mean(
-        operator_errors[:, :, i * batch_size:(i + 1) * batch_size], axis=2)
+for nb in range(number_of_batches):
+    matrix_errors_batches[:, :, nb] = np.mean(
+        matrix_errors[:, :, nb * batch_size:(nb + 1) * batch_size], axis=2
+    )  #average error over runs divided into batches for each number of data points and each type of observable
 #now we calculate the average and standard deviation of each batch
-operator_errors_average = np.mean(operator_errors_batches, axis=2)
-operator_errors_std = np.std(operator_errors_batches, axis=2, ddof=1)
+matrix_errors_average = np.mean(matrix_errors_batches, axis=2)
+matrix_errors_std = np.std(matrix_errors_batches, axis=2, ddof=1)
 #the batch averages can be interpreted as being Gaussian for large number of runs
 #so we can calculate the confidence intervals using student's t-distribution
 #we define the t_value for 95% confidence and 9 degrees of freedom
-t_value = stats.t.ppf((1 + 0.95) / 2, number_of_batches - 1)
-operator_errors_confidence_interval = t_value * operator_errors_std / np.sqrt(
+t_value = stats.t.ppf((1 + confidence_level) / 2, number_of_batches - 1)
+matrix_errors_confidence_interval = t_value * matrix_errors_std / np.sqrt(
     number_of_batches)
 
 #error plots
@@ -113,11 +114,11 @@ operator_errors_confidence_interval = t_value * operator_errors_std / np.sqrt(
 
 # error plots
 plt.figure()
-plt.loglog(data_points_number, operator_errors_average)
+plt.loglog(data_points_number, matrix_errors_average)
 
 # plot confidence intervals as shaded regions
-lower_bound = operator_errors_average - operator_errors_confidence_interval
-upper_bound = operator_errors_average + operator_errors_confidence_interval
+lower_bound = matrix_errors_average - matrix_errors_confidence_interval
+upper_bound = matrix_errors_average + matrix_errors_confidence_interval
 plt.fill_between(data_points_number,
                  lower_bound[:, 0],
                  upper_bound[:, 0],
@@ -133,20 +134,20 @@ plt.fill_between(data_points_number,
 plt.loglog(
     data_points_number,
     np.power(np.float64(data_points_number), -1) *
-    operator_errors_average[0, 1] /
+    matrix_errors_average[0, 1] /
     np.power(np.float64(data_points_number[0]), -1))
 plt.loglog(
     data_points_number,
     np.power(np.float64(data_points_number), -0.5) *
-    operator_errors_average[0, 1] /
+    matrix_errors_average[0, 1] /
     np.power(np.float64(data_points_number[0]), -0.5))
-plt.xlabel('number of data points')
+plt.xlabel('number of data points M')
 
 # plot legends
 plt.legend([
-    'Gaussian operator error', 'Monomial operator error', 'slope -1',
-    'slope -0.5'
+    'Gaussian error', 'Monomial error', 'CI Gaussian', 'CI Monomial',
+    '$M^{-1}$', '$M^{-0.5}$'
 ])
-plt.title('log-log-plot of error of operators vs number of observables')
+plt.title('log-log-plot of error in matrix norm vs number of observables')
 plt.show(block=True)
 1 - 1
