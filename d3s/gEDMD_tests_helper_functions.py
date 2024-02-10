Got remaining figures and fixed calculation if gaussians to work for all dimensions

d3s/gEDMD_tests_helper_functions.py
@@ -426,7 +426,7 @@ gedmd_helper.plot_errors_dictionary_limit(min_number_of_dictionary_functions,
     # get finer and finer mesh for gaussians
     boxes_list = [np.array(Omega._boxes)]
     Omega_list = [Omega]
-    observables_numbers = [boxes_list[-1][0] * boxes_list[-1][1]]
+    observables_numbers = [Omega.numBoxes()]
     while observables_numbers[-1] < max_number_of_observables:
         boxes_list.append(
             np.array([
@@ -434,7 +434,7 @@ gedmd_helper.plot_errors_dictionary_limit(min_number_of_dictionary_functions,
                 for number in multiplier * np.array(boxes_list[-1])
             ]))
         Omega_list.append(domain.discretization(Omega._bounds, boxes_list[-1]))
-        observables_numbers.append(boxes_list[-1][0] * boxes_list[-1][1])
+        observables_numbers.append(Omega_list[-1].numBoxes())
 
     number_of_loops_observables = len(observables_numbers)
 

gEDMDtests/gEDMD_Dictionary_Limit_All.py
@@ -26,7 +26,7 @@ min_number_of_observables = 2
 boxes1 = 2
 boxes2 = 2
 boxes = np.array([boxes1, boxes2])
-max_number_of_observables = 100
+max_number_of_observables = 1000
 
 number_of_runs = 50
 number_of_batches = 5
@@ -128,8 +128,8 @@ gedmd_helper.plot_errors_dictionary_limit(
 # ########################################
 
 # define domain
-bounds = np.array([[-2, 2]])
-boxes = np.array([max_number_of_observables])
+bounds = np.array([[-1, 1]])
+boxes = np.array([2])
 Omega = domain.discretization(bounds, boxes)
 
 # define system