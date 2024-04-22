5c81cdb7d5368342faa1a94d55bb286fc83acb13
Liam Llamazares <s2266126@ed.ac.uk>
2024-04-22 18:34:14 +0100

    Deleted unused function

--- d3s/observables.py
+++ d3s/observables.py
@@ -299,6 +299,21 @@ class FEM_2d(object):
                                 n1 * _np.arange(1, n2) + n1 - 1,
                                 n1 * (n2 - 1) + _np.arange(1, n1 - 1)))
 
+    def __get_triangles(self):
+        '''
+        Returns array of triangles, where a triangle is a tuple of 3 indices of nodes. There are 2*(n1-1)*(n2-1) triangles.
+        '''
+        n1 = self.n1
+        n2 = self.n2
+        triangles = _np.empty((2 * (n1 - 1) * (n2 - 1), 3), dtype=int)
+        for j in range(n2 - 1):  # Triangles in row j
+            for i in range(n1 - 1):  # Triangles in column i
+                triangles[2 * (n1 - 1) * j +
+                          2 * i] = _np.array([0, 1, n1 + 1]) + j * n1 + i
+                triangles[2 * (n1 - 1) * j + 2 * i +
+                          1] = _np.array([0, n1, n1 + 1]) + j * n1 + i
+        return triangles
+
     def __get_Triangles(self, X):
         '''
         Find the triangles that contain the points in X.
